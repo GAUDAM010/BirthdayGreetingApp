107673d7c72a0e10467a5dcb08fbeef63f402a24
GAUDAM010 <gaudamvaibav.e2022@vitstudent.ac.in>
2024-06-20 18:21:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/birthdaygreetingapp/BirthdayGreetingActivity.kt
@@ -0,0 +1,13 @@
+// BirthdayGreetingActivity.kt
+package com.example.birthdaygreetingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+    }
+}
---
+++ app/src/main/java/com/example/birthdaygreetingapp/MainActivity.kt
@@ -0,0 +1,21 @@
+// MainActivity.kt
+package com.example.birthdaygreetingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val birthdayButton: Button = findViewById(R.id.birthdayButton)
+        birthdayButton.setOnClickListener {
+            val intent = Intent(this, BirthdayGreetingActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
